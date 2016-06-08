3b287baf880a570da0c1efd1b7875bf59215ce66
Erin O'Toole <erino2oo@gmail.com>
2016-06-08 11:07:30 -0700

    updated battleship.py
    Renamed, as well as updated variables to a bit more meaningful names

--- Battleship/battleship.py
+++ Battleship/battleship.py
@@ -109,25 +109,25 @@ def ship_hit(x, y, ships):
 				return True
 	return False
 
-def guess_hit_bitch():
-    guess_hit = raw_input("~~~~~ \nGuess where my shit is [x, y] :")
-    guess_hit.split(",")
-    if (len(guess_hit.split(",")) == 2):
-        x_str,y_str = guess_hit.split(",")
+def guess_hit():
+    guess_hit_input = raw_input("~~~~~ \nGuess where my ship is [x, y] :")
+    guess_hit_input.split(",")
+    if (len(guess_hit_input.split(",")) == 2):
+        x_str,y_str = guess_hit_input.split(",")
         try:
             guess_hit_x = int(x_str)
         except ValueError:
             print "Nice try, please enter a valid coordinate"
-            return guess_hit_bitch()
+            return guess_hit()
         try:
             guess_hit_y = int(y_str)
         except ValueError:
             print "Nice try, please enter a valid cordinate"
-            return guess_hit_bitch()
+            return guess_hit()
         return guess_hit_x,guess_hit_y
     else:
         print "Nice try, please enter a valid coordinate in form x,y :"
-        return guess_hit_bitch()
+        return guess_hit()
 
 
 #Does not work for when out of board range? FIX PLEASE
@@ -142,9 +142,9 @@ for x in range(20):
 
 number_o_ships = randint(2,5)
 
-print "~~~~~~ \nLet's play Battleshits!"
+print "~~~~~~ \nLet's play Battleship!"
 print "This is (for now) a single-player game."
-print "Good Luck, and don't fuck it up! \n~~~~~~"
+print "Good Luck, and don't mess it up! \n~~~~~~"
 print "You have", number_o_ships * 6, "guesses to hit %s ships.\n" %number_o_ships
 length = randint(1,4) # can be between 1 and 4 in length
 direction = randint(1,4) # 1 = N, 2 = E, 3 = S, 4 = W
@@ -162,14 +162,14 @@ for a in list_o_ships:
         single_list.sort()
 
 for turn in range(20):
-    guess_hit_x, guess_hit_y = guess_hit_bitch()
-    guess_hit = [guess_hit_x, guess_hit_y]
+    guess_hit_x, guess_hit_y = guess_hit()
+    guess_hit_coord = [guess_hit_x, guess_hit_y]
 
     if ship_hit(guess_hit_x, guess_hit_y, list_o_ships) == True:
-        if guess_hit not in hit_list:
+        if guess_hit_coord not in hit_list:
             print "~~~~~ \nCongratulations! You hit my battleshit! \n~~~~~"
             board[guess_hit_x][guess_hit_y] = "*"
-            hit_list.append([guess_hit[0], guess_hit[1]])
+            hit_list.append([guess_hit_coord[0], guess_hit_coord[1]])
             hit_list.sort()
             print_board(board)
             print hit_list #debug
@@ -181,13 +181,13 @@ for turn in range(20):
     #### How to know when someone sinks a ship??? ###
 
     else:
-        if (guess_hit > [len(board)-1, len(board)-1]):
+        if (guess_hit_coord > [len(board)-1, len(board)-1]): #This might be a bug, test this
             print "~~~~~ \nWhoa, buddy, keep it in your pants. \n~~~~~"
         elif (board[guess_hit[0]][guess_hit[1]] == "*"):
     	        print "~~~~~ \nBeen there, done that. \n~~~~~"
         else:
             print "~~~~~ \nHaha. NO. \n~~~~~"
-            board[guess_hit[0]][guess_hit[1]] = "X"
+            board[guess_hit_coord[0]][guess_hit_coord[1]] = "X"
         print_board(board)
 
     if hit_list == single_list:
